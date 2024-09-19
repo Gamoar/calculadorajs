6ed7d34bc89f77689102fc2b78934c1769eaa48c
Gabriel Monteiro Araujo <151694575+Gamoar@users.noreply.github.com>
2024-09-18 23:48:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+let vari1;
+let vari2;
+let visor;
+
+function somar(vari1,vari2){
+    vari1 = parseInt(document.getElementById("var1").value);
+    vari2 = parseInt(document.getElementById("var2").value);
+    visor = vari1 + vari2;
+    document.getElementById("visor").value = visor;
+}
+
+function diminuir(vari1,vari2){
+    vari1 = parseInt(document.getElementById("var1").value);
+    vari2 = parseInt(document.getElementById("var2").value);
+    visor = vari1 - vari2;
+    document.getElementById("visor").value = visor;
+}
+
+function multiplicar(vari1,vari2){
+    vari1 = parseInt(document.getElementById("var1").value);
+    vari2 = parseInt(document.getElementById("var2").value);
+    visor = vari1 * vari2;
+    document.getElementById("visor").value = visor;
+}
+
+function dividir(vari1,vari2){
+    vari1 = parseInt(document.getElementById("var1").value);
+    vari2 = parseInt(document.getElementById("var2").value);
+    visor = vari1 / vari2;
+    document.getElementById("visor").value = visor;
+}
+
+function limpar(vari1,vari2){
+    visor = 0;
+    document.getElementById("var1").value = "";
+    document.getElementById("var2").value = "";
+    document.getElementById("visor").value = visor;
+}
